20e6190adc3141f99cb88335dd30a59ca17874f4
jnike01 <bekennedy09@gmail.com>
2023-07-03 19:34:57 -0400

    pc in start menu implementation

--- src/pokemon_storage_system.c
+++ src/pokemon_storage_system.c
@@ -1668,13 +1668,22 @@ static void FieldTask_ReturnToPcMenu(void)
     u8 taskId;
     MainCallback vblankCb = gMain.vblankCallback;
 
-    SetVBlankCallback(NULL);
-    taskId = CreateTask(Task_PCMainMenu, 80);
-    gTasks[taskId].tState = 0;
-    gTasks[taskId].tSelectedOption = sPreviousBoxOption;
-    Task_PCMainMenu(taskId);
-    SetVBlankCallback(vblankCb);
-    FadeInFromBlack();
+    if (FlagGet(FLAG_POKEMONPCMENU)==TRUE)
+	{
+		SetVBlankCallback(NULL);
+		taskId = CreateTask(Task_PCMainMenu, 80);
+		gTasks[taskId].tState = 0;
+		gTasks[taskId].tSelectedOption = sPreviousBoxOption;
+		Task_PCMainMenu(taskId);
+		SetVBlankCallback(vblankCb);
+		FadeInFromBlack();
+	}
+	else {
+		//ScriptContext2_Disable(); // useless but unidentified
+		//EnableBothScriptContexts(); // useless but inidentified
+		SetVBlankCallback(CB2_ReturnToField);
+		FadeInFromBlack();
+	}
 }
 
 #undef tState
